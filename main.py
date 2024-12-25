2a131c5aee4908d3de7c60749fa6b5ad1c502b33
jungwoo3490 <jungwoo3490@naver.com>
2024-12-26 03:40:18 +0900

    fix: fix logging path

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     config = get_config()
 
     logging.basicConfig(
-        filename='../logs/app.log',
+        filename='logs/app.log',
         level=logging.INFO,
         format='%(asctime)s - %(levelname)s: %(message)s'
     )
